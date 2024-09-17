b3759a601ecfa236d56322657bb61bc2f38a9138
SMujtabA11 <mailooficial12@gmail.com>
2024-09-17 12:44:06 -0700

    hero secion has been created with animation

---
+++ index.js
@@ -0,0 +1,29 @@
+gsap.fromTo("#hero-img img",
+    { y: 10 },
+    { 
+      y: 0, 
+      duration: 2, 
+      yoyo: true, 
+      repeat: -1 ,
+    }
+  );
+
+  gsap.from("#hero-text h1 ,button ",{
+    x:-300,
+    duration:2,
+    delay:1,
+    stagger:1.8,
+});
+ 
+gsap.from("#logo-div ", {
+    x:-300,         
+    duration: 2,    
+    delay: 1,   
+  });
+
+  gsap.from("#nav-links ", {
+    y:-300,         
+    duration: 2,    
+    delay: 1,   
+    stagger:2.8,
+  });
